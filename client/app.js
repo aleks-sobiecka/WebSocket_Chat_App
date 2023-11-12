42ec07173682780ebee3f74782c61d8d80f475fb
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2023-11-12 15:48:42 +0100

    Add app.js and package.json files

--- client/app.js
+++ client/app.js
@@ -1,3 +1,5 @@
+
+// HTML references
 const loginForm = document.getElementById("welcome-form");
 const messagesSection = document.getElementById("messages-section");
 const messagesList = document.getElementById("messages-list");
@@ -5,4 +7,5 @@ const addMessageForm = document.getElementById("add-messages-form");
 const userNameInput = document.getElementById("username");
 const messageContentInput = document.getElementById("message-content");
 
-let userName = userNameInput.value;
+// Global vars
+let userName = '';
